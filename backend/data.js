d3edd9f55ee091f2ad1ce4ece80248e785ca7ad4
youngtalento <karthikaam18041999@gmail.com>
2021-05-12 21:25:36 +0530

    connect mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+      {
+        name: 'Karthikaa Moorthy',
+        email: 'karthikaa@admin.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: true,
+      },
+      {
+        name: 'Karthikaa',
+        email: 'karthikaa@user.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: false,
+      },
+    ],
     products: [
       {
         _id: '1',
